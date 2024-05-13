06b120d45e65e34b47f1f3f2f6b7933a6b064a2f
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-05-14 00:02:32 +0530

    Debugging

--- Connection/connection.go
+++ Connection/connection.go
@@ -30,12 +30,12 @@ func ConnectToServer() {
 func readLoop(conn net.Conn){
 	buf := make([]byte,2048)
 	for {
-		_,err := conn.Read(buf)
+		n,err := conn.Read(buf)
 		if err != nil {
 			fmt.Print("Read loop error")
 			break
 		}
-		
+		fmt.Println(string(buf[:n]))
 		i,err := strconv.Atoi(strings.TrimSpace(string(buf)[0:1]))
 		j,err:= strconv.Atoi(strings.TrimSpace(string(buf)[3:4]))
 		fmt.Println(i,j)
